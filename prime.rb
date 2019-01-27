65a69df1e0425837c3a93783f7db95feae24ed9e
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-27 02:31:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+  return false
+ end
+  (2...integer).to_a.each do |number|
+  if integer % number == 0
+   return false
+  end
+ end
+  return true
+end
